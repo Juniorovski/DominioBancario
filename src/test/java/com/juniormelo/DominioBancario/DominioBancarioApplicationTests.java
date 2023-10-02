d0cc383bc4b55e622f8fd47b286fa4d7b44177c6
juniormelo <juniornine19@gmail.com>
2023-10-02 10:19:32 -0300

    Initial commit

---
+++ src/test/java/com/juniormelo/DominioBancario/DominioBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniormelo.DominioBancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
